c3e755623d838ff5e3edda6a453a0631d52278a8
Ronin214 <ronpon214@gmail.com>
2025-05-13 09:44:36 +0400

    Add files via upload

--- install.py
+++ install.py
@@ -8,8 +8,10 @@ os.system(f"cd {scriptDir}/yay")
 os.system("makepkg -si")
 os.system(f"rm -rf {scriptDir}/yay")
 
+os.system("yay -S zapret")
+os.system("sudo pacman -S nwg-look ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd mako nvim")
+
 os.system(f"cp -r {scriptDir} ~/.config")
 os.system(f"cd -r {scriptDir}/config /opt/zapret")
-os.system("sudo pacman -S nwg-look ttf-ubuntu-mono-nerd ttf-jetbrains-mono-nerd mako nvim")
 os.system("rm -rf ~/.config/install.py")
 os.system("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh")
